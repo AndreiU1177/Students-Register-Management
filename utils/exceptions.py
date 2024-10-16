13c0d6d1b9ba3e44c1595343ee6577a3dcfd564b
Andrei Ungureanu <77896002+AndreiU1177@users.noreply.github.com>
2024-10-16 20:39:23 +0300

    Add files via upload

---
+++ utils/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidInput(Exception):
+    pass
+
+class InvalidId(Exception):
+    pass
+
+class InvalidGrade(Exception):
+    pass
+
+class InvalidDiscipline(Exception):
+    pass
+
+class UndoRedoException(Exception):
+    pass
